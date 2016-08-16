5e8cf6fc3823cd87ff9feacfe0b337f7a4c94861
deng <978548481@qq.com>
2016-08-16 14:23:11 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/hj',function (req, res, next) {
-  res.render('hj', { data:{} });
+  res.render('hj', { sn:"_LC_ps3_non_2214982214711570731406128_SX",publishtime: "2016-08-14 14:44:34",relateid: 26841504,uid: "22149822",exp: 932278,
+    level: 24,usign: "40e2e62c4421a0013d62ca3712f7f2ae"});
 });
 router.post('/dm',function (req, res, next) {
   console.log(JSON.stringify(req.body));
